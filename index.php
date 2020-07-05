62095be25baf7f56e648bae0491b67f59552fada
Matt <mattrpittman@gmail.com>
2020-07-05 15:18:51 -0600

    4th

--- index.php
+++ index.php
@@ -3,22 +3,22 @@
         // $api_key = 078f24647204e62bf274992bc5bf8e43;
         // api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key}
 
-        // create & initialize a curl session
-        $curl = curl_init();
+        // // create & initialize a curl session
+        // $curl = curl_init();
 
-        // set our url with curl_setopt()
-        curl_setopt($curl, CURLOPT_URL, "api.example.com");
+        // // set our url with curl_setopt()
+        // curl_setopt($curl, CURLOPT_URL, "api.example.com");
 
-        // return the transfer as a string, also with setopt()
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        // // return the transfer as a string, also with setopt()
+        // curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-        // curl_exec() executes the started curl session
-        // $output contains the output string
-        $output = curl_exec($curl);
+        // // curl_exec() executes the started curl session
+        // // $output contains the output string
+        // $output = curl_exec($curl);
 
-        // close curl resource to free up system resources
-        // (deletes the variable made by curl_init)
-        curl_close($curl);
+        // // close curl resource to free up system resources
+        // // (deletes the variable made by curl_init)
+        // curl_close($curl);
 
 // function callAPI($method, $url, $data)
 // {
