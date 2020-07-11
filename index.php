163a5806620a7885dcb8136347637ef08b75766b
Matt <mattrpittman@gmail.com>
2020-07-10 22:31:00 -0600

    calculated farahnheit

--- index.php
+++ index.php
@@ -1,59 +1,62 @@
 <?php
-        $zip = 57783;
-        $country = 'us';
+$zip = 57783;
+$country = 'us';
 
 
-        $api = $_ENV["api_key"];
+$api = $_ENV["api_key"];
 
-        $api_url = 'api.openweathermap.org/data/2.5/weather' .
-                '?zip=' . $zip . ',' .
-                $country . '&appid=' . $api;
+$api_url = 'api.openweathermap.org/data/2.5/weather' .
+        '?zip=' . $zip . ',' .
+        $country . '&appid=' . $api;
 
-        // Initialize cURL
-        $ch = curl_init();
+// Initialize cURL
+$ch = curl_init();
 
-        // Set Options
+// Set Options
 
-        // URL Request
-        curl_setopt($ch, CURLOPT_URL, $api_url);
+// URL Request
+curl_setopt($ch, CURLOPT_URL, $api_url);
 
-        // Return instead of outputting directly
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// Return instead of outputting directly
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-        // Whether to include the header in the output. Set to false here
-        curl_setopt($ch, CURLOPT_HEADER, 0);
+// Whether to include the header in the output. Set to false here
+curl_setopt($ch, CURLOPT_HEADER, 0);
 
-        // 3 Execute the request and fetch the response, check for errors
-        $output = curl_exec($ch);
+// 3 Execute the request and fetch the response, check for errors
+$output = curl_exec($ch);
 
-        $weather = json_decode($output, true);
-        // 4. Close and free up the curl handle
-        curl_close($ch);
-        $temp_k = $weather["main"]["temp"];
-        $temp_f = ($temp_k - 273.15) * 9/5 + 32;
+$weather = json_decode($output, true);
+// 4. Close and free up the curl handle
+curl_close($ch);
+$temp_k = $weather["main"]["temp"];
+$temp_f = ($temp_k - 273.15) * 9 / 5 + 32;
 
-                
-                // api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key}
 
-               
+// api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key}
 
-        
+
+
+
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php weather</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>php weather</title>
 </head>
+
 <body>
-    <p>Zip Code: <?php echo $zip; ?></p>
-    <p>Temperature: <?php echo $temp_f; ?></p>
-    <!-- <p>Temperature: <?php print_r($temp); ?></p> -->
-    <!-- <p>URL: <?php echo $api_url; ?></p> -->
-    <p><?php echo $output; ?></p>
-    
+        <p>Zip Code: <?php echo $zip; ?></p>
+        <p>Temperature: <?php echo $temp_f; ?>&#8457;</p>
+        <!-- <p>Temperature: <?php print_r($temp); ?></p> -->
+        <!-- <p>URL: <?php echo $api_url; ?></p> -->
+        <p><?php echo $output; ?></p>
+
 </body>
+
 </html>
