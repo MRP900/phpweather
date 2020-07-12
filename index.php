81fd3ea697283bbaa019b7c282e94b6871781299
Matt <mattrpittman@gmail.com>
2020-07-11 19:47:39 -0600

    test post

--- index.php
+++ index.php
@@ -12,36 +12,38 @@ if (!empty($_POST)) {
 // POST: Sanitize, set action
 if (isset($_POST)) {
     $zip = filter_input(INPUT_POST, 'zip', FILTER_SANITIZE_STRING);
-        // Initialize cURL
-        $ch = curl_init();
-
-        // Set Options
-        // URL Request
-        curl_setopt($ch, CURLOPT_URL, $api_url);
-
-        // Return instead of outputting directly
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-        // Whether to include the header in the output. Set to false here
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-
-        // 3 Execute the request and fetch the response, check for errors
-        $output = curl_exec($ch);
-
-        $weather = json_decode($output, true);
-        // 4. Close and free up the curl handle
-        curl_close($ch);
-
-        // Get values from JSON string, convert kelvin to Fahrenheit
-        $temp_k = $weather["main"]["temp"];
-        $temp_f = round(($temp_k - 273.15) * 9 / 5 + 32, 1);
-
 } 
 
 
+// Build API url
+$api = $_ENV["api_key"];
+$api_url = 'api.openweathermap.org/data/2.5/weather' .
+		   '?zip=' . $zip . ',' .
+		    $country . '&appid=' . $api;
 
+// Initialize cURL
+$ch = curl_init();
 
+// Set Options
+// URL Request
+curl_setopt($ch, CURLOPT_URL, $api_url);
 
+// Return instead of outputting directly
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// Whether to include the header in the output. Set to false here
+curl_setopt($ch, CURLOPT_HEADER, 0);
+
+// 3 Execute the request and fetch the response, check for errors
+$output = curl_exec($ch);
+
+$weather = json_decode($output, true);
+// 4. Close and free up the curl handle
+curl_close($ch);
+
+// Get values from JSON string, convert kelvin to Fahrenheit
+$temp_k = $weather["main"]["temp"];
+$temp_f = round(($temp_k - 273.15) * 9 / 5 + 32, 1);
 
 
 ?>
@@ -51,30 +53,30 @@ if (isset($_POST)) {
 <html lang="en">
 
 <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>php weather</title>
-        <link rel="stylesheet" href="style/bootstrap.min.css">
-        <link rel="stylesheet" href="style/styles.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>php weather</title>
+	<link rel="stylesheet" href="style/bootstrap.min.css">
+	<link rel="stylesheet" href="style/styles.css">
 </head>
 
 <body>
 
-<form action="." method="post">
-<label>Enter Zip Code:</label>
-<input type="text" name="zip">
+	<form action="." method="post">
+		<label>Enter Zip Code:</label>
+		<input type="text" name="zip">
 
-<div class="form-group">
-<!-- <input type="hidden" name="action" value="display-weather"> -->
-<input type="submit" value="Display Weather" class="btn btn-outline-dark">
-</div>
-</form>
+		<div class="form-group">
+			<!-- <input type="hidden" name="action" value="display-weather"> -->
+			<input type="submit" value="Display Weather" class="btn btn-outline-dark">
+		</div>
+	</form>
 
-<div id="output-div">
-<h3>Displaying weather for Zip Code: <?php echo $zip ?></h3>
-<p>Temperature: <?php echo $temp_f; ?> &#8457;</p>
-<p><?php echo $output; ?></p>
-</div>
+	<div id="output-div">
+		<h3>Displaying weather for Zip Code: <?php echo $zip ?></h3>
+		<p>Temperature: <?php echo $temp_f; ?> &#8457;</p>
+		<p><?php echo $output; ?></p>
+	</div>
 
 </body>
 
