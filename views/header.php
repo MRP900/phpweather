a178baa0d52db8332fd96ec2e5e16315499a97b7
Matt <mattrpittman@gmail.com>
2020-07-24 15:15:46 -0600

    moved script after styles in header

--- views/header.php
+++ views/header.php
@@ -6,12 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php weather</title>
 
+    <link rel="stylesheet" href="styles/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/styles.css">
+
     <script>
         window.onload = () => {
         document.getElementById('input-zip').focus().select();
         }
     </script>
-
-    <link rel="stylesheet" href="styles/bootstrap.min.css">
-    <link rel="stylesheet" href="styles/styles.css">
 </head>
