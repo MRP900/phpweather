cdaa00fa41df98f327d643c62ce8f232e61a2af5
Matt <mattrpittman@gmail.com>
2020-07-23 23:33:26 -0600

    implementing mvc

--- index.php
+++ index.php
@@ -1,69 +1,31 @@
 <?php
+include 'Models/functions.php';
 
-// Default location data
-if (!isset($zip)) {
-$zip = null;
-}
-// Us as default country
-$country = 'us';
-// Array to hold errors
-$error = null;
 
 //Check for POST
 if (!empty($_POST)) {
 $_POST = array_map('trim', $_POST);
 }
 
+			// // POST: Sanitize, set action
+			// if (isset($_POST['action'])) {
+			// 	$action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
+			// } else if (isset($_GET['action'])) {
+			// 	$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+			// } else {
+			// 	$action = 'list-pets';
+			// }
+
+// Default location data
+if (!isset($zip)) {
+	$zip = null;
+}
+
 // POST: Sanitize, set action
 if (!empty($_POST)) {
 	$zip = filter_input(INPUT_POST, 'zip', FILTER_SANITIZE_STRING);
 
-	if ((strlen($zip) < 5)) {
-	$error = "Error: Zip code is too Short";
-	// Regex to check for letters
-	} elseif ($zip) {
-		// Build API url
-		$api_key = $_ENV["api_key"];
-		$api_url = 'api.openweathermap.org/data/2.5/weather' .
-		'?zip=' . $zip . ',' .
-		$country . '&appid=' . $api_key;
-
-		// Initialize cURL
-		$ch = curl_init();
-
-		// Set Options
-		// URL Request
-		curl_setopt($ch, CURLOPT_URL, $api_url);
-
-		// Return instead of outputting directly
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-		// Whether to include the header in the output. Set to false here
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-
-		// 3 Execute the request and fetch the response, check for errors
-		$output = curl_exec($ch);
-
-		if (curl_errno($ch)) {
-		$error = 'Request Error: ' . curl_error($ch);
-		} else {
-		$weather = json_decode($output, true);
-		// 4. Close and free up the curl handle
-		curl_close($ch);
-
-		// Get values from JSON string, convert kelvin to Fahrenheit
-		$temp_k = $weather["main"]["temp"];
-		$temp_f = round(($temp_k - 273.15) * 9 / 5 + 32, 1);
-
-		// Town
-		$town = $weather["name"];
-		// Humidity
-		$humidity = $weather["main"]["humidity"];
-		// Wind
-		$wind = $weather["wind"]["speed"];
-		}
-
-	}
+	get_weather($zip);
 }
 
 ?>
@@ -89,10 +51,10 @@ if (!empty($_POST)) {
 <body>
 	<div class="container">
 		<div class="col-lg mx-auto text-center">
-			<h2 id="title">Weather Lookup</h2>
+			<h2 id="title">Weather Zip</h2>
 			<form class="form-group align-content-center" action="." method="post">
 
-				<input id="input-zip" type="text" name="zip" placeholder="Enter a 5 digit Zip Code">
+				<input id="input-zip" type="text" name="zip" placeholder="Enter Zip Code">
 
 				<div class="form-group">
 					<input type="submit" value="Display Weather" class="btn btn-outline-dark">
@@ -117,4 +79,3 @@ if (!empty($_POST)) {
 	</div>
 </body>
 
-</html>
