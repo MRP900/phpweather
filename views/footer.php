92b587eb7fe7c5e87ea4656300019a90468831de
Matt <mattrpittman@gmail.com>
2020-07-24 14:34:29 -0600

    add footer

--- views/footer.php
+++ views/footer.php
@@ -1,8 +1,6 @@
 <footer>
     <script>
-        window.onload = () => {
-            document.getElementById('input-zip').focus().select();
-        }
+        
 
         if (window.history.replaceState) {
             window.history.replaceState(null, null, window.location.href);
