6b12f64ae559a49d182725393a9ee2db7915813f
Matt <mattrpittman@gmail.com>
2020-07-24 15:33:04 -0600

    order styles

--- views/header.php
+++ views/header.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php weather</title>
 
-    <link rel="stylesheet" href="styles/bootstrap.min.css">
     <link rel="stylesheet" href="styles/styles.css">
-
+    <link rel="stylesheet" href="styles/bootstrap.min.css">
+    
     <script>
         window.onload = () => {
         document.getElementById('input-zip').focus().select();
