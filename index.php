4e286819a30f218257564b68785d57242baae92b
Matt <mattrpittman@gmail.com>
2020-07-25 15:09:54 -0600

    testing problems

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if ($action === 'show-weather') {
 	$zip = filter_input(INPUT_POST, 'zip', FILTER_SANITIZE_STRING);
 	$weather = array();
 	$error = "";
-	// $state = "";
+	$state = "";
 
 	// Zip Code Validation
 	if (validate_zip_code($zip)) {
