840ff790152d3d5cd050e09eba07d287a2ed4096
Matt <mattrpittman@gmail.com>
2020-07-25 21:23:42 -0600

    fixing get_state()

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ if ($action === 'show-weather') {
 	// Zip Code Validation
 	if (validate_zip_code($zip)) {
 		$weather = get_weather($zip);
-		$state = get_state($zip, $states);
+		$state = get_state($zip);
 	}
 	else {
 		$error = "Error: Zip Code must be five numbers!";
