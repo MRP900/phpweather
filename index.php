995ef6b2d18c24be8f26e57348f555b126639e1c
Matt <mattrpittman@gmail.com>
2020-11-09 20:21:29 -0700

    refined comments

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <?php
 include 'functions/functions.php';
 
-
 //Check for POST
 if (!empty($_POST)) {
-$_POST = array_map('trim', $_POST);
+	$_POST = array_map('trim', $_POST);
 }
 
 // POST: Sanitize, set action
 if (isset($_POST['action'])) {
 	$action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
-} else if (isset($_GET['action'])) {
-	$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
 } else {
 	$action = 'lookup';
 }
